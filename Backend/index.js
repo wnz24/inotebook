0cc43a981ac02f05a3502858bad3b4fdee7b4432
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2023-09-21 18:51:38 +0500

    project setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,20 @@
 const connectToMongo = require('./db');
-connectToMongo();
 const express = require('express')
+connectToMongo();
+
 const app = express()
 const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
 
 
